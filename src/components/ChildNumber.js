94e057afd868daefa0d87f242fb8c759b23d71b1
vijaygenius123 <vijaygenius123@gmail.com>
2022-05-16 22:44:29 +0100

    Add memo to stop child rerendering

--- src/components/ChildNumber.js
+++ src/components/ChildNumber.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 const ChildNumber = (props) => {
   return (
@@ -9,4 +9,4 @@ const ChildNumber = (props) => {
 };
 ChildNumber.whyDidYouRender = true;
 
-export default ChildNumber;
+export default memo(ChildNumber);
