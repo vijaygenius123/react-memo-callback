fb292eed4e79e1ebfb3d1d0f2fc516bf3380e70a
vijaygenius123 <vijaygenius123@gmail.com>
2022-05-16 22:48:55 +0100

    Add useCallback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './wdyr';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import ChildNumber from './components/ChildNumber';
 import './App.css';
 
 const App = () => {
   const [localNumber, setLocalNumber] = useState(0);
   const [childNumber, setChildNumber] = useState(0);
+
+  const memoizedCallback = useCallback((num) => changeChildNumber(num), []);
 
   function incrementLocalNumber() {
     setLocalNumber((prev) => prev + 1);
@@ -20,7 +22,7 @@ const App = () => {
     <div className="App">
       <h1>Local Number: {localNumber}</h1>
       <button onClick={incrementLocalNumber}>Increment Local Number</button>
-      <ChildNumber number={childNumber} />
+      <ChildNumber number={childNumber} changeChildNumber={memoizedCallback} />
     </div>
   );
 };
--- src/components/ChildNumber.js
+++ src/components/ChildNumber.js
@@ -1,9 +1,14 @@
 import React, { memo } from 'react';
 
 const ChildNumber = (props) => {
+  const changeNumber = () => {
+    props.changeChildNumber(Math.random());
+  };
+
   return (
     <>
       <h2>Child Number: {props.number}</h2>
+      <button onClick={changeNumber}> Change Child Number </button>
     </>
   );
 };
